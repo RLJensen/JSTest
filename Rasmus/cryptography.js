fa3f7fbc5cdac7e9fb33068479ff1dbea1fe716c
RaJensen120 <102486363+RLJensen@users.noreply.github.com>
2022-04-21 15:29:51 +0200

    new commit

--- Rasmus/cryptography.js
+++ Rasmus/cryptography.js
@@ -1,3 +1,40 @@
-function cryptography(){
+const message = 'Test new encryption'
+
+function encrypt(message) {
+    const {createCipheriv, randomBytes} = require('crypto');
     
+    const key = randomBytes(32);
+    const iv = randomBytes(16);
+
+    const cipher = createCipheriv('aes256', key, iv);
+
+    const encryptedMessage = cipher.update(message, 'utf8', 'hex') + cipher.final('hex');
+    console.log(`Encrypted message: ${encryptedMessage}`);
+
+    var object = { 
+        key: key, 
+        iv: iv, 
+        cipheredMessage: encryptedMessage
+    }
+    return object;
 }
+
+function decrypt(obj) {
+
+    const {createDecipheriv} = require('crypto');
+
+    const decipher = createDecipheriv('aes256', obj.key, obj.iv);
+    const decryptedMessage = decipher.update(obj.cipheredMessage, 'hex', 'utf-8') + decipher.final('utf8');
+    console.log(`Deciphered message: ${decryptedMessage.toString('utf-8')}`);
+}
+
+
+var obj = encrypt(message);
+decrypt(obj);
+
+
+/*
+let ht = new hashTable();
+
+he
+*/
