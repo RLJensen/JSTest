81932c44463692b42d12b38d882f6a1c26b64ef4
Rasmus Jensen <raserasedj@gmail.com>
2022-04-22 12:30:09 +0200

    new commit

--- test.js
+++
@@ -1,9 +0,0 @@
-var arr = [];
-var len = oFullResponse.results.length;
-for (var i = 0; i < len; i++) {
-    arr.push({
-        key: oFullResponse.results[i].label,
-        sortable: true,
-        resizeable: true
-    });
-}
